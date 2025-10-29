021a1be749b29707c55cba066e10b83b2d459d53
AlexAksioma <alexmedqwerty@gmail.com>
2025-10-29 19:06:17 +0200

    created retry analyser, PropertiesReader

--- src/test/java/ui_tests/StartUITests.java
+++ src/test/java/ui_tests/StartUITests.java
@@ -1,12 +1,14 @@
 package ui_tests;
 
 import manager.AppManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.BasePage.*;
 import pages.HomePage;
 import pages.en_pages.EnHomePage;
 import pages.en_pages.EnPhotoPage;
 import pages.ru_pages.RuEventsPage;
+import utils.RetryAnalyzer;
 import utils.enums.HeaderMenuItemsEn;
 import utils.enums.HeaderMenuItemsRu;
 
@@ -21,11 +23,12 @@ public class StartUITests extends AppManager {
         RuEventsPage ruEventsPage = clickRuHeaderBtn(HeaderMenuItemsRu.EVENTS);
     }
 
-    @Test
+    @Test(retryAnalyzer = RetryAnalyzer.class)
     public void startUITest1(){
         HomePage homePage = new HomePage(getDriver());
         pause(5);
         EnHomePage enHomePage = clickRuHeaderBtn(HeaderMenuItemsRu.BTN_CHANGE_LANGUAGE_RU);;
         EnPhotoPage enPhotoPage = clickEnHeaderBtn(HeaderMenuItemsEn.PHOTO);
+        Assert.fail();
     }
 }
