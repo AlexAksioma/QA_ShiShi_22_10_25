a65f7cfda0690a86171ef09526cf63eb233a76d3
ViLevin <vilevinqa@gmail.com>
2025-11-04 21:47:22 +0200

    test_click

--- src/test/java/ui_tests/vi_levin/HomeTests.java
+++ src/test/java/ui_tests/vi_levin/HomeTests.java
@@ -1,12 +1,27 @@
 package ui_tests.vi_levin;
 
+import manager.AppManager;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.HomePage;
+import pages.ru_pages.RuContactsPage;
+import utils.TestNGListener;
+import utils.enums.HeaderMenuItemsRu;
+import static pages.BasePage.clickRuHeaderBtn;
+import static pages.BasePage.pause;
 
-public class HomeTests {
+@Listeners(TestNGListener.class)
+
+public class HomeTests extends AppManager {
+
+    private SoftAssert softAssert;
 
     @Test
     public void firstTest(){
-
+            HomePage homePage = new HomePage(getDriver());
+    pause(5);
+    RuContactsPage ruContactsPage = clickRuHeaderBtn(HeaderMenuItemsRu.CONTACTS);
     }
 
 }
