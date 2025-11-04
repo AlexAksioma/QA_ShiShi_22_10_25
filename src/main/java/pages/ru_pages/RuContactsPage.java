89f3fa9f6e6959ee8e81ea6288a331713a58da56
לב לאונטייב <152332597+ZLA964@users.noreply.github.com>
2025-11-04 23:41:51 +0200

    ruContacts_Test()

--- src/main/java/pages/ru_pages/RuContactsPage.java
+++ src/main/java/pages/ru_pages/RuContactsPage.java
@@ -1,6 +1,8 @@
 package pages.ru_pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import pages.BasePage;
@@ -13,9 +15,17 @@ public class RuContactsPage extends BasePage {
                 new AjaxElementLocatorFactory(driver, 20), this);
     }
 
+    @FindBy(xpath = "//div[@class='large-8 cell']/h2[text()='ПИШИТЕ ПИСЬМА']")
+    private WebElement heardForm;
 
+    @FindBy(xpath = "//span[text()='Контактная информация']")
+    private WebElement line1;
 
+    public boolean isLine1Displayed(){
+        return isElementDisplayed(line1);
+    }
 
-
-
+    public boolean isHeardFormDisplayed(){
+        return isElementDisplayed(heardForm);
+    }
 }
--- src/test/java/ui_tests/lev_leon/HomeTests_lev.java
+++ src/test/java/ui_tests/lev_leon/HomeTests_lev.java
@@ -2,11 +2,13 @@ package ui_tests.lev_leon;
 
 
 import manager.AppManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 import pages.ru_pages.RuAboutUsPage;
-import pages.ru_pages.RuEventsPage;
+import pages.ru_pages.RuContactsPage;
 import utils.enums.HeaderMenuItemsRu;
 
 import static pages.BasePage.clickRuHeaderBtn;
@@ -14,11 +16,18 @@ import static pages.BasePage.pause;
 
 public class HomeTests_lev extends AppManager {
 
-    @Test
-    public  void AboutUs_Test(){
+    HomePage homePage;
+
+    @BeforeMethod
+    public void ruHomePage(){
         HomePage homePage = new HomePage(getDriver());
         pause(1);
+    }
+
+    @Test
+    public  void ruAboutUs_Test(){
         RuAboutUsPage ruAboutUsPage = clickRuHeaderBtn(HeaderMenuItemsRu.ABOUT_US);
+
         SoftAssert softAssert = new SoftAssert();
 
         softAssert.assertTrue(ruAboutUsPage.isPressHeaderDisplayed(), "Заголовок 'Пресса о нас' не отображается");
@@ -26,5 +35,12 @@ public class HomeTests_lev extends AppManager {
         softAssert.assertTrue(ruAboutUsPage.isTeamHeaderDisplayed(), "Заголовок 'Команда' не отображается");
 
         softAssert.assertAll();
+    }
+
+    @Test
+    public void ruContacts_Test(){
+        RuContactsPage ruContactsPage = clickRuHeaderBtn(HeaderMenuItemsRu.CONTACTS);
+        Assert.assertTrue(ruContactsPage.isHeardFormDisplayed() &&
+                ruContactsPage.isLine1Displayed());
     }
 }
